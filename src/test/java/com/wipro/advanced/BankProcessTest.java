e7aa859efad149c90adbfb4789458b4474fdc5ac
VithiBhar <BharVithi@gmail.com>
2017-05-06 14:08:21 +0200

    testing on  saturday

---
+++ src/test/java/com/wipro/advanced/BankProcessTest.java
@@ -0,0 +1,19 @@
+package com.wipro.advanced;
+
+//import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BankProcessTest {
+
+	@Test
+	public void testTransferFunds() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBalanceOf() {
+//		fail("Not yet implemented");
+	}
+
+}
